661b8014bf14ee79e1203db018e4f2f2a88f81a2
makk9 <makkenaa9@gmail.com>
2024-11-25 09:25:46 -0500

    Updated websocket host IP

--- client/ui-control-dashboard/src/VideoFeed.js
+++ client/ui-control-dashboard/src/VideoFeed.js
@@ -5,7 +5,7 @@ const VideoFeed = () => {
 
     useEffect(() => {
         // Connect to the WebSocket
-        const ws = new WebSocket("ws://192.168.1.100:8765");
+        const ws = new WebSocket("ws://0.0.0.0:8765");
 
         ws.onmessage = (message) => {
             // Decode the binary data into a base64 image string
